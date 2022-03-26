a012844ff849fcaab5c48e25c572828963da24f5
sakshitomar20 <tomarsakshi86@gmail.com>
2022-03-26 15:35:42 +0530

    Angular Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { SpecialComponent } from './special/special.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"special",component:SpecialComponent},
+  {path:"contact",component:ContactUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SpecialComponent } from './special/special.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactUsComponent,
+    FooterComponent,
+    HeaderComponent,
+    SpecialComponent
   ],
   imports: [
     BrowserModule,
